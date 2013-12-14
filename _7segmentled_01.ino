de289f3e73c92d41ce743964acee4c03d999d92e
erickramer3000 <eric.kramer@gmail.com>
2013-12-14 08:32:14 -0500

    it works. refresh rate and duration are now a parameter for the double-digit \'rendering\' function.

--- _7segmentled_01.ino
+++ _7segmentled_01.ino
@@ -1,10 +1,14 @@
-/* _7segmentled_01
+/* 
+   _7segmentled_01:
+   
    1.0: Driving the right digit of a 2-digit 7-segment led common anode display.
    1.1: Now going to try shutting off the display once in a while with an NPN transistor.
    1.2: Now trying to have one digit displaying while the other isn't.  Added another xistor.
    1.3: Now that I have the right segments in series between left-digit and right-digit,
         I want to switch between digits enabled and digits displayed fast enough to give the 
         illusion that both digits are always on.
+   1.4: 1.3 worked surprisingly well but now I want to support a real refresh rate and an 
+        adjustable one at that.
 */
 
 byte seven_seg_digits[10][7] = {
@@ -28,8 +32,8 @@ void setup() {
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);  // right digit transistor control
-  pinMode(10, OUTPUT); // left digit transistor control
+  pinMode(9, OUTPUT);  // right digit enable pin
+  pinMode(10, OUTPUT); // left digit enable pin
   digitalWrite(9, HIGH); // i want rd high right away
   digitalWrite(10, LOW); // i want ld low right away
 }
@@ -42,29 +46,34 @@ void sevenSegWrite(byte digit) {
   }
 }
 
-void render2Digits(byte inputNumber) {
+void render2Digits(byte inputNumber, int duration, int refreshRate) {
   // keeps the number between 0 inclusive and 99 inclusive.
   if (inputNumber > 99) {
     inputNumber = 99;
   } else if (inputNumber < 0) {
     inputNumber = 0;
   }
-  int quotient10 = inputNumber / 10;
-  int remainder10 = inputNumber % 10;
-  for (byte intervals = 3; intervals > 0; --intervals) {
+  
+  int halfDigitDuration = (duration * 1000 / refreshRate) / 2;
+  
+  int quotient10 = inputNumber / 10; // left digit is the 10s column
+  int remainder10 = inputNumber % 10; // right digit is the 1s column
+  
+  for (int intervals = refreshRate; intervals > 0; --intervals) {
     digitalWrite(9, LOW);
     digitalWrite(10, HIGH); 
     sevenSegWrite((byte) quotient10);
-    delay(166); // 1/6th of a second
+    delay(halfDigitDuration); 
     digitalWrite(9, HIGH);
     digitalWrite(10, LOW); 
     sevenSegWrite((byte) remainder10);
-    delay(166); // 1/6th of a second
+    delay(halfDigitDuration); 
   }
+}
 
 void loop() {
   for (byte count = 99; count > 0; --count) {
-    render2Digits(count);
+    render2Digits(count, 1, 60);
   }
-  delay(3000);
+  render2Digits(0, 3, 60);
 }
