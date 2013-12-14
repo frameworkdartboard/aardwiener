165bed68629b356fdb9dcffeab236b6f9db93a0a
erickramer3000 <eric.kramer@gmail.com>
2013-12-14 08:14:20 -0500

    it\'s already counting in the right order and the approximate 1 second durations is maintained even though 166 * 6.s isn\'t really 1000 ms.

--- _7segmentled_01.ino
+++ _7segmentled_01.ino
@@ -1,6 +1,10 @@
 /* _7segmentled_01
    1.0: Driving the right digit of a 2-digit 7-segment led common anode display.
    1.1: Now going to try shutting off the display once in a while with an NPN transistor.
+   1.2: Now trying to have one digit displaying while the other isn't.  Added another xistor.
+   1.3: Now that I have the right segments in series between left-digit and right-digit,
+        I want to switch between digits enabled and digits displayed fast enough to give the 
+        illusion that both digits are always on.
 */
 
 byte seven_seg_digits[10][7] = {
@@ -24,8 +28,10 @@ void setup() {
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT); // transistor control
-  digitalWrite(9, HIGH); // i want it high right away
+  pinMode(9, OUTPUT);  // right digit transistor control
+  pinMode(10, OUTPUT); // left digit transistor control
+  digitalWrite(9, HIGH); // i want rd high right away
+  digitalWrite(10, LOW); // i want ld low right away
 }
 
 void sevenSegWrite(byte digit) {
@@ -36,16 +42,29 @@ void sevenSegWrite(byte digit) {
   }
 }
 
+void render2Digits(byte inputNumber) {
+  // keeps the number between 0 inclusive and 99 inclusive.
+  if (inputNumber > 99) {
+    inputNumber = 99;
+  } else if (inputNumber < 0) {
+    inputNumber = 0;
+  }
+  int quotient10 = inputNumber / 10;
+  int remainder10 = inputNumber % 10;
+  for (byte intervals = 3; intervals > 0; --intervals) {
+    digitalWrite(9, LOW);
+    digitalWrite(10, HIGH); 
+    sevenSegWrite((byte) quotient10);
+    delay(166); // 1/6th of a second
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW); 
+    sevenSegWrite((byte) remainder10);
+    delay(166); // 1/6th of a second
+  }
+
 void loop() {
-  for (byte count = 10; count > 0; --count) {
-    delay(1000);
-    // i wonder if i'll see odd numbers or even numbers because of this trick?
-    if (count % 2 == 0) {
-      digitalWrite(9, LOW);
-    } else {
-      digitalWrite(9, HIGH);
-    }
-    sevenSegWrite(count - 1);
+  for (byte count = 99; count > 0; --count) {
+    render2Digits(count);
   }
   delay(3000);
 }
